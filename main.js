4851334c89f2b2bb0ef8311296f24c53f78bad97
NikitaNpl <totmyanin.n@bk.ru>
2021-02-27 18:49:13 +0300

    [edit] - ревью кода

--- main.js
+++ main.js
@@ -1,62 +1,53 @@
 
 $(function () {
-  let isSelectedBackgorundColor = true;
+  const init = () => {
+    const refreshColor = getRefreshFunction();
 
-  $("input").checkboxradio({
-    icon: false
-  });
+    $(".red, .green, .blue").slider({
+      orientation: "horizontal",
+      range: "min",
+      max: 255,
+      value: 127,
+      slide: refreshColor,
+      change: refreshColor
+    });
+
+
+    $(".red").slider("value", 255);
+    $(".green").slider("value", 140);
+    $(".blue").slider("value", 60);
+    $("input").checkboxradio({
+      icon: false
+    });
+  }
+
+  let isSelectedBackgroundColor = $("input:radio:checked").attr("id") === "bgColor";
+
+  const getRefreshFunction = (() => {
+    const refreshColor = (prop) => () => {
+      const red = $(".red").slider("value"),
+          green = $(".green").slider("value"),
+                blue = $(".blue").slider("value");
+      $("#text-block").css(prop, `rgb(${red}, ${green}, ${blue})`);
+    }
+
+    const refreshBackgroundColor = refreshColor("background-color");
+    const refreshTextColor = refreshColor("color");
+    return () => isSelectedBackgroundColor ? refreshBackgroundColor : refreshTextColor;
+  })();
 
   $("input").on("change", function(){
-    isSelectedBackgorundColor = !isSelectedBackgorundColor
+    isSelectedBackgroundColor = !isSelectedBackgroundColor;
+    const refreshColor = getRefreshFunction();
+
     $(".red, .green, .blue").slider({
-      slide: isSelectedBackgorundColor ? refreshBackgroundColor : refreshColor,
-      change: isSelectedBackgorundColor ? refreshBackgroundColor : refreshColor
+      slide: refreshColor,
+      change: refreshColor
     });
   });
 
-
-  function hexFromRGB(r, g, b) {
-    var hex = [
-      r.toString(16),
-      g.toString(16),
-      b.toString(16)
-    ];
-    $.each(hex, function (nr, val) {
-      if (val.length === 1) {
-        hex[nr] = "0" + val;
-      }
-    });
-    return hex.join("").toUpperCase();
-  }
-
-  function refreshBackgroundColor() {
-    var red = $(".red").slider("value"),
-      green = $(".green").slider("value"),
-      blue = $(".blue").slider("value"),
-      hex = hexFromRGB(red, green, blue);
-    $("#text-block").css("background-color", "#" + hex);
-  }
-
-  function refreshColor() {
-    var red = $(".red").slider("value"),
-      green = $(".green").slider("value"),
-      blue = $(".blue").slider("value"),
-      hex = hexFromRGB(red, green, blue);
-    $("#text-block").css("color", "#" + hex);
-  }
-
-  $(".red, .green, .blue").slider({
-    orientation: "horizontal",
-    range: "min",
-    max: 255,
-    value: 127,
-    slide: isSelectedBackgorundColor ? refreshBackgroundColor : refreshColor,
-    change: isSelectedBackgorundColor ? refreshBackgroundColor : refreshColor
-  });
-
-  $(".red").slider("value", 255);
-  $(".green").slider("value", 140);
-  $(".blue").slider("value", 60);
+  init();
+  
 });
 
 
